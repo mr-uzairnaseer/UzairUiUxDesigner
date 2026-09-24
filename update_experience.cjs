9da0cec2e0800970f61dea2b39a9152429e45fc6
uzair@github <mr.uzairnaseerahmed@gmail.com>
2026-09-25 03:30:24 +0500

    Add distinct experience and education cards to the about section

---
+++ update_experience.cjs
@@ -0,0 +1,50 @@
+const fs = require('fs');
+let appJsx = fs.readFileSync('src/App.jsx', 'utf8');
+
+// 1. Add Briefcase to imports
+appJsx = appJsx.replace(/GraduationCap, X,/, 'GraduationCap, X, Briefcase,');
+
+// 2. Add ap-edu-cards CSS
+appJsx = appJsx.replace(/(\.ap-edu-card \{)/, '.ap-edu-cards { display: flex; flex-direction: column; gap: 24px; width: 100%; }\n  $1');
+
+// 3. Replace the single card HTML
+const oldCard = \`<div className="ap-edu-card sr-group">
+              <span className="ap-tag-pill sr-child" style={{ "--i": 0 }}>
+                <GraduationCap size={13} /> education
+              </span>
+              <h3 className="sr-child" style={{ "--i": 1 }}>BS Computer Science</h3>
+              <div className="ap-school sr-child" style={{ "--i": 2 }}>Quaid-I-Azam University<br/>Islamabad</div>
+              <div className="ap-edu-row sr-child" style={{ "--i": 3 }}>
+                <span>Graduated</span><b>2025</b>
+              </div>
+              <div className="ap-edu-row ap-edu-row-last sr-child" style={{ "--i": 4 }}>
+                <span>Experience</span><b>1 Year</b>
+              </div>
+            </div>\`;
+
+const newCards = \`<div className="ap-edu-cards sr-group">
+              <div className="ap-edu-card sr-child" style={{ "--i": 0 }}>
+                <span className="ap-tag-pill">
+                  <GraduationCap size={13} /> education
+                </span>
+                <h3>BS Computer Science</h3>
+                <div className="ap-school" style={{ marginBottom: "0" }}>Quaid-I-Azam University<br/>Islamabad, 2025</div>
+              </div>
+              <div className="ap-edu-card sr-child" style={{ "--i": 1 }}>
+                <span className="ap-tag-pill">
+                  <Briefcase size={13} /> experience
+                </span>
+                <h3>Tech Web Ninja</h3>
+                <div className="ap-school">UI/UX Designer</div>
+                <div className="ap-edu-row">
+                  <span>Duration</span><b>1 Year</b>
+                </div>
+                <div className="ap-edu-row ap-edu-row-last">
+                  <span>MMSC</span><b>3 Months (Intern)</b>
+                </div>
+              </div>
+            </div>\`;
+
+appJsx = appJsx.replace(oldCard, newCards);
+
+fs.writeFileSync('src/App.jsx', appJsx, 'utf8');
